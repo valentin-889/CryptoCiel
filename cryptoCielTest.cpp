3c382faa4c419775127842230cf568156057474f
valentin-889 <valentin.public88@gmail.com>
2024-09-19 11:01:41 +0200

    update des fichiers

--- cryptoCielTest.cpp
+++ cryptoCielTest.cpp
@@ -1,17 +1,15 @@
-// RsaCiel.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
-
 #include <iostream>
 #include "RsaGestion.h"
 #include "Hashgestion.h"
 #include "AesGestion.h"
+#include <fstream>
 
 int main()
 {
-	HashGestion LM;
-	std::string monMessageHash = "Super message superchouette";
-	std::cout << LM.CalculateSHA256(monMessageHash) << std::endl;
+    HashGestion LM;
+    std::string File = "fichier.txt";
+    std::cout << "SHA256 Hash: " << LM.CalculateSHA256(File) << std::endl;
+    return(0);
+
 
 }
-
-
